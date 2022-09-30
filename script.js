c9e1bfe7ee1746d78324bc7b615477af16f0a524
Spencer Merrill <canadianfaller7@gmail.com>
2022-09-29 21:29:50 -0600

    added condition statement to make sure the numbers are put in correctly

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const number = ["1","2","3","4","5","6","7","8","9"]
 const specialChar = ["!","@","#","$","%","^","&","*","-", "/", "."]
 const alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] 
 
-console.log(alphaUpper)
-
-
  generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -17,10 +14,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-const generatePassword = () => {
+
+// Add event listener to generate button
+
+const generatePassword = checkLength => {
+  let passwordLength = window.prompt("How many characters do you want your password to contain? ")
   
+  if (!passwordLength) {
+    alert("Password must be between 8 and 60 characters.")
+    generatePassword()
+  }
+  else if (passwordLength <= 7 || passwordLength >= 61) {
+    alert("Password must be between 8 and 60 characters.")
+    generatePassword()
+  }
+  else {
+    confirm("Do you want to have Capital letters?")
+  }
 }
