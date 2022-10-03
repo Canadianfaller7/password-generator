1bed94bcdf3d9f0c143bb9217633a4dd5712ca68
Spencer Merrill <canadianfaller7@gmail.com>
2022-10-02 20:09:03 -0600

    Set my function to variables so that way I could then refrence them as parameters inside my randomizePassword function and get the correct information that I needed to make the randomized password

--- script.js
+++ script.js
@@ -9,30 +9,24 @@ generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 const writePassword = () => {
-  generatePassword()
-  let password = passwordCriteria();
+  const passLength = generatePassword()
+  const passCriteria = passwordCriteria()
+  let password = randomizePassword(passLength, passCriteria);
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 generateBtn.addEventListener("click", writePassword);
 
-
 // Add event listener to generate button
-
 const generatePassword = () => {
   let passwordLength = prompt("How many characters do you want your password to contain? ")
   // Conditional statement using the ternary operator to see if password length is 0, less than 8 or greater than 60 and will throw an alert and ask to put in the correct criteria or will then run the passwordCriteria function
-  if(!passwordLength || passwordLength <= 7 || passwordLength >= 61) {
-    alert("Password must be between 8 and 60 characters.")
-    generatePassword()
+  while (passwordLength <= 7 || passwordLength >= 129) {
+    alert("Password must be between 8 and 128 characters")
+    passwordLength = prompt("How many characters do you want your password to contain? ")
   }
-  else{
-    return passwordLength;
-  }
-  
-  
+  return passwordLength
 }
 
 
@@ -100,31 +94,27 @@ const passwordCriteria = () => {
   return passCombo
 }
 
-const randomizePassword = () => {
+const randomizePassword = (passLength, passCriteria) => {
 
   let randomPassword = []
   
-  for (let i = 0; i < passwordLength; i++) {
-    let newPassword = passCombo[Math.floor(Math.random() * passCombo.passwordLength())]
+  for (let i = 0; i < passLength; i++) {
+    let newPassword = passCriteria[Math.floor(Math.random() * passCriteria.length - 1)]
     randomPassword.push(newPassword)
   }
-  let finalPassword = randomPassword.join()
-
+  let finalPassword = randomPassword.join("")
   return finalPassword;
 }
 
-
-
-
 /* 
-1. Ask the user to put in the amount of characters they want in their password and veryify if it meets the standards of 8 to 60 characters
- if password is 0, return an alert and if password is smaller than 8 or larger than 60 characters return an alert error else move on to step 2.
 
-2. Ask the user if they would like to have capital letters in their password, lowercase, numbers, or special characters
+  1. Ask the user to put in the amount of characters they want in their password and veryify if it meets the standards of 8 to 60 characters
+  if password is 0, return an alert and if password is smaller than 8 or larger than 60 characters return an alert error else move on to step 2.
 
-3. Once have answers, make some conditional statements on whether they chose yes or no for the questions in step 2  to see if they selected all the items, none, or only some of them and then join the arrays into 1
+  2. Ask the user if they would like to have capital letters in their password, lowercase, numbers, or special characters
 
-4. 
+  3. Once have answers, make some conditional statements on whether they chose yes or no for the questions in step 2  to see if they selected all the items, none, or only some of them and then join the arrays into 1
 
+  4. Take the information that was joined into 1 array and then run it through a for loop and then randomize the array to the length of what the user put in and then return it and pass it into our password variable 
 
 */
